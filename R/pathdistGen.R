13096230d83dca32d66abd296749e09a1bfa7ed6
jsta <jstachelek@utexas.edu>
2016-03-01 21:38:49 -0500

    lint pathdistGen.R

--- R/pathdistGen.R
+++ R/pathdistGen.R
@@ -11,75 +11,73 @@
 #'@importFrom utils setTxtProgressBar
 #'@export
 #'@examples
-#'spdf<-data.frame(rnorm(2))
-#'xy<-data.frame(x=c(4,2),y=c(8,4))
-#'coordinates(spdf)<-xy
+#'spdf <- data.frame(rnorm(2))
+#'xy <- data.frame(x = c(4, 2), y = c(8, 4))
+#'coordinates(spdf) <- xy
 #'
-#'m<-matrix(NA,10,10)
-#'costras<-raster(m,xmn=0,xmx=ncol(m),ymn=0,ymx=nrow(m))
-#'costras[]<-runif(ncell(costras),min=1,max=10)
+#'m <- matrix(NA, 10, 10)
+#'costras <- raster(m, xmn = 0, xmx = ncol(m), ymn = 0, ymx = nrow(m))
+#'costras[] <- runif(ncell(costras), min = 1, max = 10)
 #'#introduce spatial gradient
 #'for(i in 1:nrow(costras)){
-#'costras[i,]<-costras[i,]+i
-#'costras[,i]<-costras[,i]+i
+#'  costras[i,] <- costras[i,] + i
+#'  costras[,i] <- costras[,i] + i
 #'}
 #'
-#'rstack<-pathdistGen(spdf,costras,100)
+#'rstack <- pathdistGen(spdf, costras, 100)
 
-
-
-'pathdistGen'<-function(spdf,costras,range,yearmon="default"){
+'pathdistGen'<-function(spdf, costras, range, yearmon = "default"){
   
   if(class(spdf)!="SpatialPointsDataFrame"){
     stop("spdf object must be of class SpatialPointsDataFrame")
   }
   
-  ipdw.range<-range/raster::res(costras)[1]/2 #this is a per cell distance
+  ipdw.range <- range/raster::res(costras)[1] / 2 #this is a per cell distance
   #print(ipdw.range)
   
   #start interpolation#####
   #calculate conductances hence 1/max(x)
-  trans<-gdistance::transition(costras,function(x)1/max(x),directions=16)
-  i=1
-  coord<-spdf[i,]
+  trans <- gdistance::transition(costras, function(x) 1 / max(x), directions = 16)
+  i <- 1
+  coord <- spdf[i,]
   #print(coord)
   #print(coordinates(coord))
   #print(coord@data)
-  A<-gdistance::accCost(trans,coord)
-  dist<-hist(A,plot=F)$breaks[2]
-  if(dist<ipdw.range){
-    dist=ipdw.range    
+  A <- gdistance::accCost(trans, coord)
+  dist <- hist(A,plot = F)$breaks[2]
+  if(dist < ipdw.range){
+    dist = ipdw.range    
   }
   
-  pb<-utils::txtProgressBar(max=nrow(spdf),style=3)
+  pb <- utils::txtProgressBar(max = nrow(spdf), style = 3)
   
     for(i in 1:nrow(spdf)){
-      coord<-spdf[i,]
+      coord <- spdf[i,]
       #print(coord)
       #print(coordinates(coord))
       #print(coord@data)
-      A<-gdistance::accCost(trans,coord)
-      A2<-raster::reclassify(A,c(dist,+Inf,NA,ipdw.range,dist,ipdw.range)) #raster cells are 1 map unit
-      A3<-((ipdw.range/A2)^2)
-      A4<-raster::reclassify(A3,c(-Inf,1,0))
+      A <- gdistance::accCost(trans, coord)
+      A2 <- raster::reclassify(A, c(dist, +Inf, NA, ipdw.range, dist, ipdw.range)) #raster cells are 1 map unit
+      A3 <- ((ipdw.range/A2)^2)
+      A4 <- raster::reclassify(A3, c(-Inf, 1, 0))
       #print(i)
       #check output with - zoom(A4,breaks=seq(from=0,to=5,by=1),col=rainbow(5))
       #showTmpFiles()
       
-      rf<-raster::writeRaster(A4,filename=file.path(tempdir(),paste(yearmon,"A4ras",i,".grd",sep="")),overwrite=T,NAflag=-99999)
-      setTxtProgressBar(pb,i)      
+      rf <- raster::writeRaster(A4, filename = file.path(tempdir(), paste(yearmon, "A4ras", i, ".grd", sep = "")), overwrite = T, NAflag = -99999)
+      setTxtProgressBar(pb, i)      
     }
   close(pb)
      
   
   #create raster stack
-  raster_data<-list.files(path=file.path(tempdir()),pattern=paste(yearmon,"A4ras*",sep=""),full.names=T)
-  raster_data<-raster_data[grep(".grd",raster_data,fixed=T)]
-  as.numeric(gsub('.*A4ras([0123456789]*)\\.grd$','\\1',raster_data))->fileNum
-  raster_data<-raster_data[order(fileNum)]
-  rstack<-raster::stack(raster_data)
-  rstack<-raster::reclassify(rstack,cbind(-99999,NA))
-  file.remove(list.files(path=file.path(tempdir()),pattern=paste(yearmon,"A4ras*",sep=""),full.names=T))
+  raster_data <- list.files(path = file.path(tempdir()), pattern = paste(yearmon, "A4ras*", sep = ""), full.names = T)
+  raster_data <- raster_data[grep(".grd", raster_data, fixed = T)]
+  as.numeric(gsub('.*A4ras([0123456789]*)\\.grd$', '\\1', raster_data)) -> fileNum
+  raster_data <- raster_data[order(fileNum)]
+  rstack <- raster::stack(raster_data)
+  rstack <- raster::reclassify(rstack, cbind(-99999, NA))
+  file.remove(list.files(path = file.path(tempdir()), pattern = paste(yearmon, "A4ras*", sep = ""), full.names = T))
   
   return(rstack)
 }
