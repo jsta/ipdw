7417a9f21ca2816a3fe54ab3909e293130e6361f
jsta <jstachelek@utexas.edu>
2016-07-25 13:06:45 -0400

    fix line break expectation to pass tests

--- R/costrasterGen.R
+++ R/costrasterGen.R
@@ -44,8 +44,8 @@
   
   #add check to see if projstr and projection(pols) match
   if(!identical(projstr, sp::proj4string(pols))){
-    message("Warning, the projection of polygons does not match projstr.
-    				See rgdal::spTransform")
+    message("Warning, the projection of polygons does not
+    				match projstr. See rgdal::spTransform")
   }
   
   #define spatial domain based on pnts or polys
