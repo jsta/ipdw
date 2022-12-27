be735f4c1639c44102dcb588f9cbee502568b190
jsta <stachel2@msu.edu>
2022-12-27 16:54:27 -0700

    linting, [skip ci]

--- data-raw/kattgat.R
+++ data-raw/kattgat.R
@@ -18,19 +18,19 @@ pols9 <- Polygons(list(Polygon(kattegat$dk[9])), "pol9")
 pols10 <- Polygons(list(Polygon(kattegat$dk[10])), "pol10")
 pols11 <- Polygons(list(Polygon(kattegat$dk[11])), "pol11")
 pols12 <- Polygons(list(Polygon(kattegat$dk[12])), "pol12")
-pols <- SpatialPolygons(list(pols1, pols2, pols3, pols4, pols5, pols6, 
-														 pols7, pols8, pols9, pols10, pols11, pols12), 1:12)
+pols <- SpatialPolygons(list(pols1, pols2, pols3, pols4, pols5, pols6,
+  pols7, pols8, pols9, pols10, pols11, pols12), 1:12)
 dt            <- data.frame(id = row.names(pols))
 row.names(dt) <- row.names(pols)
 pols          <- SpatialPolygonsDataFrame(data = dt, pols)
 projection(pols) <- katproj
 
-rgdal::writeOGR(pols, dsn = "inst/extdata/kattegat_coast.gpkg", 
-								layer = "pol", driver = "GPKG", overwrite_layer = TRUE)
+rgdal::writeOGR(pols, dsn = "inst/extdata/kattegat_coast.gpkg",
+  layer = "pol", driver = "GPKG", overwrite_layer = TRUE)
 
 pnts <- data.frame(id = seq_len(nrow(kattegat$coords)), salinity = kattegat$data)
 pnts <- cbind(pnts, kattegat$coords)
 coordinates(pnts) <- ~ x.utm + y.utm
 projection(pnts)  <- katproj
-rgdal::writeOGR(pnts, dsn = "inst/extdata/kattegat_pnts.gpkg", 
-								layer = "pnts", driver = "GPKG", overwrite_layer = TRUE)	
+rgdal::writeOGR(pnts, dsn = "inst/extdata/kattegat_pnts.gpkg",
+  layer = "pnts", driver = "GPKG", overwrite_layer = TRUE)
