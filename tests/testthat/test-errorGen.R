70e22a5f2894ed6fa8285bcdc4ec99077ac680e8
jsta <stachel2@msu.edu>
2022-12-28 11:41:53 -0700

    use sf_ob

--- tests/testthat/test-errorGen.R
+++ tests/testthat/test-errorGen.R
@@ -4,16 +4,16 @@ test_that("errorGen works", {
 
   validation.data <- data.frame(rnorm(10, mean = 0.2, sd = 1))
   names(validation.data) <- c("validation")
-  validation.spdf <- validation.data
+  validation.sf_ob <- validation.data
   validation.data <- as.numeric(unlist(validation.data))
   xy <- data.frame(x = c(0:9), y = rep(1, 10))
-  coordinates(validation.spdf) <- xy
+  validation.sf_ob <- st_as_sf(cbind(validation.sf_ob, xy), coords = c("x", "y"))
 
   m <- matrix(1, 1, 10)
   out.ras <- raster::raster(m, xmn = 0, xmx = ncol(m), ymn = 0, ymx = nrow(m))
   out.ras[] <- validation.data + rnorm(ncell(out.ras), mean = 0.01,
     sd = 0.2)
-  valid.stats <- errorGen(out.ras, validation.spdf, validation.data,
+  valid.stats <- errorGen(out.ras, validation.sf_ob, validation.data,
     plot = FALSE, title = "Validation Plot")
   expect_equal(length(valid.stats), 2)
 
